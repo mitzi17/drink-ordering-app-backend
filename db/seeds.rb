82ec90ed20c5c4cefa501b5f889cde5d85dee5ab
Mitzi17 <m_estrada17@live.com>
2021-06-29 23:37:32 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+order_one = Order.create(date: Date.today.prev_day)
+order_two = Order.create(date: Date.today)
+
+drink_one = order_one.drinks.create(base: 'milk tea', flavor: 'none', size: 'large', add_on: 'honey boba')
+drink_two = order_one.drinks.create(base: 'jasmine green tea', flavor: 'honey', size: 'medium', add_on: 'honey boba')
+drink_three = order_two.drinks.create(base: 'jasmine green tea', flavor: 'passion fruit', size: 'large', add_on: 'none')
