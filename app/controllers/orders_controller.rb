c96f73432784fcefd29c15c1dcc39caa60c8b0fc
Mitzi17 <m_estrada17@live.com>
2021-06-30 00:38:30 -0700

    add controller actions and strong params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+
+    def index
+        @orders = Order.all
+        render json: @orders
+    end
+
+    def create
+        @order = Order.new(order_params)
+        if @order.save
+            render json: @order
+        else 
+            render json: {error: "Error: Order not created"}
+        end
+    end
+
+    def show
+        @order = Order.find(params[:id])
+        render json: @order
+    end
+
+    def destroy
+        @order = Order.find(params[:id])
+        @order.destroy
+    end
+
+    private
+
+    def order_params
+        params.require(:order).permit(:date)
+    end
+
 end
